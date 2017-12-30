172da7cfa783b1c55570d76ea3c3fc6fc48c20df
Ken Price <kenaprice@gmail.com>
2017-12-29 22:36:36 -0500

    Delete TODO shit god dammit fuck
    Writing code while drinking lol fuck you

--- BAMFSHE.ino
+++ BAMFSHE.ino
@@ -2,13 +2,6 @@
 #include "game.h"
 #include "globals.h"
 
-// TODO: Delete after sprite shit done
-void drawTiles() {
-  for (unsigned char i = 0; i < 128; i++) {
-    arduboy.drawBitmap((i / 8) * 8, (i * 8) % 64, SPRITES + (i * 8), 8, 8, WHITE);
-  }
-}
-
 typedef void (*FunctionPointer) ();
 const FunctionPointer PROGMEM mainGameLoop[] =
 {
@@ -33,8 +26,6 @@ void setup() {
 }
 
 void loop() {
-  // gameState = 0; // TODO: Delete after sprite shit done
-  
   if (!(arduboy.nextFrame())) return;
   arduboy.pollButtons();
   arduboy.clear();
